6e284d1b631304f4b9dee1dc97f81b4e80268a84
Nataliia Hordiienko <Natalihordienko@gmail.com>
2025-01-18 05:35:57 +0100

    del user

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,7 +2,6 @@ import createHttpError from "http-errors";
 import bcrypt   from "bcrypt";
 import {randomBytes} from "crypto";
 
-import UserCollection from "../db/models/user.js";
 import SessionCollection from "../db/models/session.js";
 
 import { accessTokenLifeTime, refreshTokenLifeTime } from "../constants/auth.js";
